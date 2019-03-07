a728b0cec1513814f9d0b527470e5fef5f5cc23f
at-hangtran <hang.tran@asiantech.vn>
2019-03-07 22:08:07 +0700

    merge json

---
+++ src/test/java/vn/asiantech/core/ReportListener.java
@@ -0,0 +1,89 @@
+package vn.asiantech.core;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.testng.ITestContext;
+import org.testng.TestListenerAdapter;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * ReportListener
+ */
+public class ReportListener extends TestListenerAdapter {
+    private File[] files;
+
+    @Override
+    public final void onFinish(final ITestContext testContext) {
+        super.onFinish(testContext);
+        try {
+            mergeJsonFiles();
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void mergeJsonFiles() throws IOException, ParseException {
+        int size;
+        files = getAllFiles();
+        List<String> resultIds = new ArrayList<>();
+        JSONArray resultFeatures;
+        if (files.length > 0) {
+            resultFeatures = getFeatures(new FileReader(files[0].getPath()));
+            size = resultFeatures.size();
+            for (int i = 0; i < size; i++) {
+                resultIds.add((String) ((JSONObject) resultFeatures.get(i)).get("id"));
+            }
+        } else {
+            return;
+        }
+
+        for (int pos = 1; pos < files.length; pos++) {
+            JSONArray fileFeatures = getFeatures(new FileReader(files[pos].getPath()));
+            for (int i = 0; i < size; i++) {
+                for (Object fileFeature : fileFeatures) {
+                    JSONObject mergeObject = (JSONObject) resultFeatures.get(i);
+                    JSONObject object = (JSONObject) fileFeature;
+                    String idMergeFeature = (String) mergeObject.get("id");
+                    String idFeature = (String) object.get("id");
+                    if (idMergeFeature.equals(idFeature)) {
+                        JSONArray mainElements = (JSONArray) mergeObject.get("elements");
+                        JSONArray elements = (JSONArray) object.get("elements");
+                        mainElements.addAll(elements);
+                    } else if (!resultIds.contains(idFeature)) {
+                        resultIds.add(idFeature);
+                        resultFeatures.add(fileFeature);
+                    }
+                }
+            }
+        }
+        Files.write(Paths.get(files[0].getPath()), resultFeatures.toJSONString().getBytes());
+        System.out.println("Merge json reports success.");
+        deleteFiles();
+    }
+
+    private JSONArray getFeatures(final FileReader file) throws IOException, ParseException {
+        JSONParser parser = new JSONParser();
+        Object object = parser.parse(file);
+        return (JSONArray) object;
+    }
+
+    private File[] getAllFiles() {
+        File dir = new File("target/cucumber-reports/");
+        return dir.listFiles((dir1, name) -> name.endsWith(".json"));
+    }
+
+    private void deleteFiles() {
+        for (int i = 1; i < files.length; i++) {
+            files[i].delete();
+        }
+    }
+}
